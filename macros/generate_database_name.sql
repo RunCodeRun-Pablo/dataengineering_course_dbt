cbf598993d04738f63a644276247c64f6bbbe474
Pablo Martin <pablomarlop98@gmail.com>
2026-04-30 17:54:14 +0000

    solved problem in dbt_project.yml

--- macros/generate_database_name.sql
+++ macros/generate_database_name.sql
@@ -1,8 +1,5 @@
-{% macro generate_database_name(
-custom_database_name, node
-) -%}
-    {%-if custom_database_name
-    is not none -%}
+{% macro generate_database_name(custom_database_name, node) -%}
+    {%-if custom_database_name is not none -%}
     {{ custom_database_name | trim }}
     {%-else -%}
     {{ target.database | trim }}
