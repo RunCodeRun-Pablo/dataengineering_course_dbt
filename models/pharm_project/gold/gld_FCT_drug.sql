f92927b8ee30561143e98cdb0d0239cedbea269f
Pablo Martin <pablomarlop98@gmail.com>
2026-05-06 11:19:24 +0000

    Finished with __orders.yml for silver layer. Based on tests made, new surrogate key was created for slv_drug. slv_drug_disease was therefore not necessary

--- models/pharm_project/gold/gld_FCT_drug.sql
+++ models/pharm_project/gold/gld_FCT_drug.sql
@@ -1,25 +1,18 @@
-WITH ref1 AS (
+WITH ref AS (
 
     SELECT * FROM {{ref("slv_drug")}}
 
-),
-
-ref2 AS (
-
-    SELECT * FROM {{ref('slv_drug_disease')}}
 ),
 
 
 gld_data AS (
 
     SELECT
-        d.id_sponsor,
-        d.id_drug_name,
-        dis.id_disease,
-        d.price_per_dose
-    FROM ref1 d
-    LEFT JOIN ref2 dis
-    ON d.id_app_numb = dis.id_app_numb
+        id_sponsor,
+        id_drug_name,
+        id_disease,
+        price_per_dose
+    FROM ref
 )
 
 SELECT * FROM gld_data
--- models/pharm_project/silver/slv_drug.sql
+++ models/pharm_project/silver/slv_drug.sql
@@ -9,10 +9,11 @@ source as (
 slv_drug AS (
     
     SELECT
-        {{dbt_utils.generate_surrogate_key(['app_numb'])}} AS id_app_numb,
+        {{dbt_utils.generate_surrogate_key(['app_numb', 'drug_name'])}} AS id_app_numb,
         app_numb,
         {{dbt_utils.generate_surrogate_key(['sponsor_name'])}} AS id_sponsor,
         {{dbt_utils.generate_surrogate_key(['drug_name'])}} AS id_drug_name,
+        {{dbt_utils.generate_surrogate_key(['disease'])}} AS id_disease,
         price_per_dose
     FROM source
 
